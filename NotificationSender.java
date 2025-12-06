ff32329db0874ea6f76b356d0a6776758cf6ed3f
qNVDV <stuqnada@gmail.com>
2025-12-06 15:56:45 +0200

    Delete NotificationSender.java

--- NotificationSender.java
+++
@@ -1,7 +0,0 @@
-/**
- * Interface demonstrating Interfaces & Polymorphism.
- */
-public interface NotificationSender {
-    void sendAssignmentNotification(Bug bug, String assignedBy);
-    void sendCompletionNotification(Bug bug, String completedBy);
-}
